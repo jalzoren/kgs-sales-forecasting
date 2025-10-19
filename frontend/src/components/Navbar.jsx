c60f858d67e39955b32ed808afeb167cc46e5c7f
jalzoren <flavierlaurence01@gmail.com>
2025-10-20 00:11:29 +0800

    u

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,43 +8,23 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-top">
         <div className="logo">
-          <div className="logo">
-            <FaBullseye className="logo-icon" />
-            <span className="logo-text">Sales Forecasting System</span>
-          </div>
+          <FaBullseye className="logo-icon" />
+          <span className="logo-text">Sales Forecasting System</span>
         </div>
 
         <ul className="navbar-links">
-          <li>
-            <NavLink to="/home">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/data">Data</NavLink>
-          </li>
-          <li>
-            <NavLink to="/forecast">Forecast</NavLink>
-          </li>
-          <li>
-            <NavLink to="/reports">Reports</NavLink>
-          </li>
-          <li>
-            <NavLink to="/analytics">Analytics</NavLink>
-          </li>
+          <li><NavLink to="/home">Home</NavLink></li>
+          <li><NavLink to="/data">Data</NavLink></li>
+          <li><NavLink to="/forecast">Forecast</NavLink></li>
+          <li><NavLink to="/reports">Reports</NavLink></li>
+          <li><NavLink to="/analytics">Analytics</NavLink></li>
         </ul>
 
         <div className="navbar-right">
-          <button className="icon-btn">
-            <FaBell />
-          </button>
-          <button className="logout-btn">
-            
-            <LiaUserCircle />
-
-          </button>
+          <button className="icon-btn"><FaBell /></button>
+          <button className="logout-btn"><LiaUserCircle /></button>
         </div>
       </div>
-
-      <br></br>
 
       <div className="navbar-stats">
         <div className="stat-item">
@@ -52,19 +32,25 @@ function Navbar() {
           <p className="value">0</p>
           <span>next 7 days</span>
         </div>
+
         <div className="divider"></div>
+
         <div className="stat-item">
           <h4>Actual Sales</h4>
           <p className="value">0</p>
           <span>previous 7 days</span>
         </div>
+
         <div className="divider"></div>
+
         <div className="stat-item">
           <h4>Forecast Accuracy</h4>
-          <p className="value green">0</p>
+          <p className="value green">0%</p>
           <span>variance: 7%</span>
         </div>
+
         <div className="divider"></div>
+
         <div className="stat-item">
           <h4>Inventory Alerts</h4>
           <p className="value red">0</p>
