381ac7938432c86b9ea707419e617f898e81c0ea
jerimiahbitancor <jerimiahbitancor27@gmail.com>
2025-10-23 22:33:08 +0800

    updated database

--- backend/kgs.sql
+++ backend/kgs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2025 at 12:26 PM
+-- Generation Time: Oct 23, 2025 at 04:31 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
