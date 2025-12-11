934d92d4a20c2b6dfa51fb6fa2c4ef42123d7193
jalzoren <flavierlaurence01@gmail.com>
2025-12-11 16:52:14 +0800

    A

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
-// backend/config/db.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,        // from .env
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
