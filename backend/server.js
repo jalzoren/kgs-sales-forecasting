8eba730f31814e9d5aff8070a04b025434fcbfb7
jalzoren <flavierlaurence01@gmail.com>
2025-12-14 22:06:03 +0800

    fix: link of cors frontend of render

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 // CORS (RENDER SAFE)
 // =======================
 app.use(cors({
-  origin: "https://your-frontend-url.onrender.com", // frontend URL
+  origin: "https://kgs-sales-forecasting-frontend.onrender.com", // frontend URL
   credentials: true
 }));
 // =======================
